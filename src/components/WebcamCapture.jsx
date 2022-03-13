438026b0a4dc75b3c860b775cc04a1643f16ffcf
DeveloperYamin <developer.yamin@gmail.com>
2022-03-13 23:52:45 +0600

    setup redux

--- src/components/WebcamCapture.jsx
+++ src/components/WebcamCapture.jsx
@@ -1,5 +1,40 @@
+import { RadioButtonUnchecked } from "@mui/icons-material";
+import { useCallback, useRef, useState } from "react";
+import Webcam from "react-webcam";
+
 function WebcamCapture() {
-  return <div>WebcamCapture</div>;
+  const webcamRef = useRef(null);
+  const [image, setImage] = useState(null);
+
+  const videoConstrainer = {
+    width: 250,
+    height: 400,
+    facingMode: "user",
+  };
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImage(imageSrc);
+  }, [webcamRef]);
+
+  return (
+    <section className="webcamCapture">
+      <Webcam
+        audio={false}
+        height={videoConstrainer.height}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={videoConstrainer.width}
+        videoConstraints={videoConstrainer}
+      />
+      <RadioButtonUnchecked
+        className="webcamCapture_button"
+        onClick={capture}
+        fontSize="large"
+      />
+      <img src={image} alt="" />
+    </section>
+  );
 }
 
 export default WebcamCapture;
