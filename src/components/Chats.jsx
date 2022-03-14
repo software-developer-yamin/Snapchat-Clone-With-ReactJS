294116f1e53b161bcf18cfb2ded5bc83f7c58a59
DeveloperYamin <developer.yamin@gmail.com>
2022-03-14 15:37:16 +0600

    chats components header structure done!

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,7 +1,18 @@
+import { ChatBubble, Search } from "@mui/icons-material";
+import { Avatar } from "@mui/material";
 import "../styles/Chats.css";
 
 function Chats() {
-  return <div>Chats</div>;
+     return <section className="chats" >
+          <header className="chats_header">
+               <Avatar className="chats_avatar" />
+               <div className="chats_search">
+                    <Search />
+                    <input type="text" />
+               </div>
+               <ChatBubble className="chats_chatIcon" />
+       </header>
+  </section>;
 }
 
 export default Chats;
