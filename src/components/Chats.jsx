a67abcf5f99718e72cc368aa6e07723d66508777
DeveloperYamin <developer.yamin@gmail.com>
2022-03-14 18:13:04 +0600

    add default style in index.css file

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -3,16 +3,21 @@ import { Avatar } from "@mui/material";
 import "../styles/Chats.css";
 
 function Chats() {
-     return <section className="chats" >
-          <header className="chats_header">
-               <Avatar className="chats_avatar" />
-               <div className="chats_search">
-                    <Search />
-                    <input type="text" />
-               </div>
-               <ChatBubble className="chats_chatIcon" />
-       </header>
-  </section>;
+  return (
+    <section className="chats">
+      <header className="chats_header">
+        <Avatar className="chats_avatar" />
+        <div className="chats_search">
+          <Search />
+          <input placeholder="Friends" type="text" />
+        </div>
+        <ChatBubble className="chats_chatIcon" />
+      </header>
+      <main className="chats_posts" >
+        
+      </main>
+    </section>
+  );
 }
 
 export default Chats;
