774654b4d5d2e1fe62ac9eae6620a184aed63e87
DeveloperYamin <developer.yamin@gmail.com>
2022-03-14 10:40:05 +0600

    add preview page structure

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,7 +1,35 @@
+import { Close } from "@mui/icons-material";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {
+  resetCameraImage,
+  selectCameraImage,
+} from "../features/camera/cameraSlice";
 import "../styles/preview.css";
 
 function Preview() {
-  return <div>Preview</div>;
+  const cameraImage = useSelector(selectCameraImage);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!cameraImage) {
+      navigate("/", { replace: true });
+    }
+  }, [cameraImage, navigate]);
+
+  const previewClose = () => {
+    dispatch(resetCameraImage());
+    navigate("/", { replace: true });
+  };
+
+  return (
+    <section className="preview">
+      <Close className="preview_close" onClick={previewClose} />
+      <img src={cameraImage} alt="Camera Image" />
+    </section>
+  );
 }
 
 export default Preview;
